f5fbd65801af949ec3233ea6a3edc843795eedd5
JD <jdwheeler0@gmail.com>
2018-11-28 22:46:47 -0600

    logging the event but still have not figured out to adjust the element.

--- event.js
+++ event.js
@@ -1,18 +1,23 @@
 const audrey = document.getElementById("audrey");
-
+const output = '';
 /*
     Add an event listener to the `document` object to listen
     for the "scroll" event.
 */
-audrey.addEventListener("scroll", function () {
+window.addEventListener("scroll", function (event) {
 
-    audrey.style.width = audrey.scrollWidth + 50px;
+    let posY = window.scrollY;
+    audrey.style.width = posY;
+    console.log(audrey);
+   
+
+    
     /*
         Adjust the width of audrey to be 1/3 the value of
         `window.scrollY`. No lower than 50px, though.
     */
 
-    audrey.style.height = audrey.scrollHeight + 100px;
+    
 
     /*  
         Adjust the height of audrey to be 1/4 the value of
